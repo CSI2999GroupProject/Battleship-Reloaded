a68611b46f5524ed03b3168c6ed9a1b184f0051e
isaac <isaac.watson28@gmail.com>
2018-02-01 22:18:24 -0500

    Basic GameBoard with minimal design. Basic Ship class(WIP) ShipInterface for new methods that should be implemented in the GameBoard

---
+++ app/src/main/java/sophomoreproject/battleship/ships/ShipInterface.java
@@ -0,0 +1,13 @@
+package sophomoreproject.battleship.ships;
+
+/**
+ * Created by isaac on 1/31/2018.
+ */
+
+public interface ShipInterface {
+
+    void addShip(Ship aShip);
+    void move();
+    void rotate();
+
+}
