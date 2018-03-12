d548d0fa78d87939ca4862b7cb9e97545e15b7f6
nisarg97 <35431077+nisarg97@users.noreply.github.com>
2018-03-12 18:49:35 -0400

    Tutorial Screen

--- app/src/main/java/sophomoreproject/battleship/MainMenu.java
+++ app/src/main/java/sophomoreproject/battleship/MainMenu.java
@@ -5,18 +5,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements
+        View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Button tutorialButton = (Button) findViewById(R.id.tutorialButton);
+        tutorialButton.setOnClickListener(this);
     }
 
     public void onDebugClick(View view)
     {
         Intent intent = new Intent(this, Debug.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.tutorialButton:
+                startActivity(new Intent(getApplicationContext(), TutorialActivity.class));
+                break;
+        }
     }
 }
